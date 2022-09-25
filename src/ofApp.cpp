d391b62cae6d75bd1ea986a0733804388d949328
Coriolan Verchezer <c.verchezer@gmail.com>
2022-09-25 12:52:11 +0100

    added a color wheel, generates the correct string

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -11,13 +11,15 @@ int numberOfRingsPairs = 1;
 #define numberOfLedsPerRing 5
 
 #define udp_incipit 0x02 // defined by WWL
-#define udp_format 0x00 // DMX style with initial break#define udp_flags 0x80 // value is 128 to start transmission immediately
+#define udp_format 0x00 // DMX style with initial break
+#define udp_flags 0x80 // value is 128 to start transmission immediately
 #define udp_ports 0x00 // 0 for same data to all ports
 #define udp_nbyteslo 0xB5 // 437 bytes per port. 437(dec) = 01B5(hex)
 #define udp_nbyteshi 0x01 // 437 bytes per port. 437(dec) = 01B5(hex)
 #define udp_nports 0x03 // number of ports in use (see hardware diagram)
 #define udp_portsinpkt 0x03 // number of ports for which this packet contains data
-char ring_strig[437] = { 0x00, globalBri, globalBri, smoothing, idByte_test };
+
+char ring_strig[437] = { 0x00, globalBri, globalBri, smoothing, idByte_test };
 
 //--------------------------------------------------------------
 void ofApp::setup(){
@@ -26,9 +28,63 @@ void ofApp::setup(){
 	udpConnection.Connect("192.168.1.78", 16661);
 	udpConnection.SetNonBlocking(true);
 
+	gui.setup();
+	//gui.add(radius.setup("radius", 140, 10, 300));
+	//gui.add(color.setup("color", ofColor(100, 100, 140), ofColor(0, 0), ofColor(255, 255)));
+	//gui.add(circleResolution.setup("circle res", 5, 3, 90));
+
+	gui.add(color.set("color", ofColor(255, 0, 0, 255), ofColor(0, 0, 0, 0), ofColor(255, 255, 255, 255)));
+	colorListener = color.newListener(this, &ofApp::updateModel);
 	
-	
+	std::cout << "setup complete" << endl;
 }
+
+//char redChar;
+void ofApp::updateModel(ofColor& color) {
+	//std::cout << "setup complete" << endl;
+	std::cout << "R,G,B,W: " << color << endl;
+
+	//int  colorHex = color.getHex();
+	//int hex = color.getHex();
+	//int red = color.r;
+	//char ofHexToChar(const string &charHexString);
+	//char red[] = ofToHex(color.r);
+	//std::cout << "hex: " << hex << endl;
+	//std::cout << "red: " << red << endl;
+	//std::cout << "ofToHex(a): " << ofToHex(color.r) << endl;
+	//char redHex = color.r;
+	//string redhex = ofToHex(color.r);
+	//char redChar = color.r;
+	//udpConnection.Send(&redChar, sizeof(redChar));
+
+	char colorRGBW[4];
+	colorRGBW[0] = color.r;
+	colorRGBW[1] = color.g;
+	colorRGBW[2] = color.b;
+	colorRGBW[3] = color.a; // I know that the alpha channel is not like White from RGB, but I prefered using the nice RGB colorwheel rather than 4 regular sliders. So for this purpose I am using the alpha as White.
+
+	std::cout << "colorRGBW[0]: " << colorRGBW[0] << endl;
+	std::cout << "colorRGBW[1]: " << colorRGBW[1] << endl;
+
+	udpConnection.Send(&colorRGBW[0], sizeof(colorRGBW[0]));
+	//udpConnection.Send(redChar, redhex.length());
+
+	for (int i = 0; i < numberOfRingsPairs*numberOfLedsPerRing * 4; i++) {
+		//i % 4 == 0 ? ring_strig[i + 5] = colorRGBW[0]:;// : ring_strig[i + 5] = 0x00;
+		//i % 4 == 1 ? ring_strig[i + 5] = 0xFF;// : ring_strig[i + 5] = 0x00;
+		//i % 4 == 2 ? ring_strig[i + 5] = 0xFF;// : ring_strig[i + 5] = 0x00;
+		//i % 4 == 3 ? ring_strig[i + 5] = 0xFF;// : ring_strig[i + 5] = 0x00;
+
+		i % 4 == 0 ? ring_strig[i + 5] = colorRGBW[0] : ring_strig[i + 5] = ring_strig[i + 5];
+		i % 4 == 1 ? ring_strig[i + 5] = colorRGBW[1] : ring_strig[i + 5] = ring_strig[i + 5];
+		i % 4 == 2 ? ring_strig[i + 5] = colorRGBW[2] : ring_strig[i + 5] = ring_strig[i + 5];
+		i % 4 == 3 ? ring_strig[i + 5] = colorRGBW[3] : ring_strig[i + 5] = ring_strig[i + 5];
+	}
+
+	udpConnection.Send(ring_strig, sizeof(ring_strig));
+
+}
+
 
 //--------------------------------------------------------------
 void ofApp::buildRingString(int color) {
@@ -37,6 +93,7 @@ void ofApp::buildRingString(int color) {
 	for (int i = 0; i < numberOfRingsPairs*numberOfLedsPerRing * 4; i++) {
 
 		// Assuming the color format is RED,GREEN,BLUE,WHITE, a loop doing everything full red, then green, then blue, then white would be:
+		// we start at 5 because the first 5 bytes are for settings, as per Mike's protocol.
 
 		if (color == 1) {
 			i % 4 == 0 ? ring_strig[i + 5] = 0xFF : ring_strig[i + 5] = 0x00;
@@ -92,13 +149,24 @@ void ofApp::testAllLEDs() {
 
 
 //--------------------------------------------------------------
+void ofApp::chooseColor() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::chooseRings() {
+
+}
+
+//--------------------------------------------------------------
 void ofApp::update(){
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	//ofDrawCircle(ofGetWidth() / 2, ofGetHeight() / 2, radius);
+	gui.draw();
 }
 
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,6 +2,7 @@
 
 #include "ofMain.h"
 #include "ofxNetwork.h"
+#include "ofxGui.h"
 
 
 class ofApp : public ofBaseApp{
@@ -15,6 +16,8 @@ class ofApp : public ofBaseApp{
 
 		void buildRingString(int color);
 		void sendUDPString();
+		void chooseColor();
+		void chooseRings();
 
 		void keyPressed(int key);
 		void keyReleased(int key);
@@ -28,7 +31,17 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 
+	ofParameter<ofColor> color;
+	void updateModel(ofColor&);
+	ofEventListener colorListener;
+
 	// Setup UDP COMM
 	ofxUDPManager udpConnection;
+
+	//ofxFloatSlider radius;
+	ofxPanel gui;
+	//ofxColorSlider color;
+	//ofxVec2Slider center;
+	//ofxIntSlider circleResolution;
 		
 };
